94225a3741bf1bcda47e8e72b7c68d6c724d7def
jvtrmx <jvitormx@gmail.com>
2025-10-29 05:09:41 -0300

    updated main.py with the proposal route

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,13 @@
 from typing import Union
 from fastapi import FastAPI
-from app.routes import usuario_routes , login_route, fornecedor_routes, requisicao_routes
+from app.routes import usuario_routes , login_route, fornecedor_routes, requisicao_routes, proposta_routes
 app = FastAPI()
 
 app.include_router(usuario_routes.router)
 app.include_router(login_route.router)
 app.include_router(fornecedor_routes.router)
 app.include_router(requisicao_routes.router)
+app.include_router(proposta_routes.router)
 
 @app.get("/")
 async def read_root():
