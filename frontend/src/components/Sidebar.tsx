38a3adf409a44f4948a5bfd9455dd8c8df62da39
Jvitormx <jvitormx@gmail.com>
2025-11-25 20:20:29 -0300

    fix ui sidebar

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -127,7 +127,7 @@ export default function Sidebar() {
           <li>
             <details className="group [&_summary::-webkit-details-marker]:hidden">
               <summary className="flex cursor-pointer items-center justify-between rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700">
-                <span className="text-sm font-medium"> Account </span>
+                <span className="text-sm font-medium"> Conta </span>
                 <span className="shrink-0 transition duration-300 group-open:-rotate-180">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
